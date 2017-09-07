a1598967c985cbf606b251327c432e4497259382
luyunhai <luyunhai@58ganji.com>
2017-09-07 10:20:00 +0800

    add project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <header-component></header-component>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import HeaderComponent from './components/Header/Header.vue';
+
+    export default {
+        name: 'app',
+        components: { HeaderComponent }
+    };
+</script>
+
+<style>
+    @import "common.less";
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+        <el-menu theme="dark" :default-active="activeIndex" class="header-menu" mode="horizontal" @select="handleSelect">
+            <div class="header-menu-logo"><img src="../../assets/logo.png" /></div>
+            <el-menu-item class="header-menu-tab" index="1">处理中心</el-menu-item>
+            <el-submenu class="header-menu-tab" index="2">
+                <template slot="title">我的工作台</template>
+                <el-menu-item class="header-menu-tab-item" index="2-1">选项1</el-menu-item>
+                <el-menu-item class="header-menu-tab-item" index="2-2">选项2</el-menu-item>
+                <el-menu-item class="header-menu-tab-item" index="2-3">选项3</el-menu-item>
+            </el-submenu>
+            <el-menu-item class="header-menu-tab" index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<style scoped lang="less">
+    @import 'Header.less';
+</style>
+
+<script>
+    export default {
+        name: 'header',
+        data () {
+            return {
+                activeIndex: '1',
+                activeIndex2: '1'
+            };
+        },
+        methods: {
+            handleSelect (key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    };
+</script>
